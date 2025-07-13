4bbfa4e8208afc31ea92fda6dad08ffbb4e455ed
Nico Devs <nico1485@gmail.com>
2025-07-13 18:05:16 -0300

    First commit

---
+++ src/languages/prism-blade.js
@@ -0,0 +1,206 @@
+import Prism from 'prismjs';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-php';
+
+const phpInside = Prism.languages.php;
+const { markup, css, javascript } = Prism.languages;
+
+const bladeEchoPatterns = {
+  'blade-echo-unescaped': {
+    pattern: /\{!!\s*[\s\S]*?\s*!!\}/,
+    greedy: true,
+    alias: 'variable',
+    inside: {
+      'symbol': {
+        pattern: /^\{!!|!!\}$/,
+        alias: 'symbol'
+      },
+      rest: phpInside
+    }
+  },
+  'blade-echo-escaped': {
+    pattern: /\{\{\s*[\s\S]*?\s*\}\}/,
+    greedy: true,
+    alias: 'variable',
+    inside: {
+      'symbol': {
+        pattern: /^\{\{|\}\}$/,
+        alias: 'symbol'
+      },
+      rest: phpInside
+    }
+  }
+};
+
+const phpInQuotes = {
+  pattern: /(["'])(?:\\[\s\S]|(?!\1)[^\\])*\1/,
+  inside: {
+    'blade': {
+      pattern: /[\s\S]+/,
+      inside: {
+        ...bladeEchoPatterns,
+        rest: phpInside
+      }
+    }
+  }
+};
+
+const phpUnquoted = {
+  pattern: /[^\s'"=<>`]+/,
+  inside: {
+    'blade': {
+      pattern: /[\s\S]+/,
+      inside: {
+        ...bladeEchoPatterns,
+        rest: phpInside
+      }
+    }
+  }
+};
+
+const createBlockPattern = (tagName, language) => ({
+  pattern: new RegExp(`<${tagName}[\\s\\S]*?>[\\s\\S]*?<\\/${tagName}>`, 'i'),
+  inside: {
+    'tag': markup.tag,
+    [`${tagName}-content`]: {
+      pattern: new RegExp(`(<${tagName}[\\s\\S]*?>)[\\s\\S]*?(?=<\\/${tagName}>)`, 'i'),
+      lookbehind: true,
+      inside: {
+        ...language,
+        ...bladeEchoPatterns
+      }
+    }
+  }
+});
+
+Prism.languages.blade = Prism.languages.extend('markup', {
+  'comment': [
+    {
+      pattern: /\{\{--[\s\S]*?--\}\}/,
+      greedy: true
+    },
+    {
+      pattern: /<!--[\s\S]*?-->/,
+      greedy: true
+    }
+  ],
+
+  'php-block': {
+    pattern: /@php\b[\s\S]*?@endphp\b/,
+    greedy: true,
+    inside: {
+      'php-start': {
+        pattern: /^@php\b/,
+        alias: 'keyword'
+      },
+      'php-end': {
+        pattern: /@endphp\b$/,
+        alias: 'keyword'
+      },
+      'php-content': {
+        pattern: /[\s\S]+/,
+        inside: phpInside
+      }
+    }
+  },
+
+  ...bladeEchoPatterns,
+
+  'blade-directive-with-params': {
+    pattern: /@[a-zA-Z_][a-zA-Z0-9_]*\s*\((?:[^()]|\([^()]*\))*\)/,
+    inside: {
+      'directive-name': {
+        pattern: /^@[a-zA-Z_][a-zA-Z0-9_]*/,
+        alias: 'keyword'
+      },
+      'directive-params': {
+        pattern: /\(([\s\S]*)\)/,
+        inside: phpInside
+      }
+    }
+  },
+
+  'blade-directive': {
+    pattern: /@[a-zA-Z_][a-zA-Z0-9_]*/,
+    alias: 'keyword'
+  },
+
+  'style-block': createBlockPattern('style', css),
+  'script-block': createBlockPattern('script', javascript),
+
+  'tag': {
+    pattern: markup.tag.pattern,
+    inside: {
+      ...markup.tag.inside,
+      'special-attr': [
+        // Dynamic (colon-prefixed) attribute: :foo="php"
+        {
+          pattern: /\s(:[\w:-]+)=("[^"]*"|'[^']*'|[^\s'"=<>`]+)/,
+          inside: {
+            'attr-name': {
+              pattern: /^(:[\w:-]+)/,
+              alias: 'attr-name'
+            },
+            'attr-value': {
+              pattern: /=((?:"[^"]*"|'[^']*'|[^\s'"=<>`]+))/,
+              inside: {
+                'punctuation': /^=/,
+                'php': {
+                  pattern: /(["'])([\s\S]*?)(["'])/,
+                  lookbehind: true,
+                  inside: phpInside
+                }
+              }
+            }
+          }
+        },
+        // Standard attribute with possible Blade echos: foo="{{ ... }}"
+        {
+          pattern: /\s([\w:-]+)=("[^"]*"|'[^']*'|[^\s'"=<>`]+)/,
+          inside: {
+            'attr-name': {
+              pattern: /^([\w:-]+)/,
+              alias: 'attr-name'
+            },
+            'attr-value': {
+              pattern: /=((?:"[^"]*"|'[^']*'|[^\s'"=<>`]+))/,
+              inside: {
+                'punctuation': /^=/,
+                'value': [
+                  phpInQuotes,
+                  phpUnquoted
+                ]
+              }
+            }
+          }
+        }
+      ]
+    }
+  },
+
+  'doctype': {
+    pattern: /<!DOCTYPE[\s\S]+?>/i,
+    greedy: true,
+    inside: {
+      'doctype-tag': /^<!DOCTYPE/i,
+      'punctuation': /[<>]/
+    }
+  },
+
+  'entity': /&#?[\da-z]{1,8};/i
+});
+
+// Recursively inject Blade echo patterns
+Prism.languages.DFS(Prism.languages.blade, (key, def) => {
+  if (def && typeof def === 'object' && def.inside) {
+    def.inside['blade-echo-escaped'] = bladeEchoPatterns['blade-echo-escaped'];
+    def.inside['blade-echo-unescaped'] = bladeEchoPatterns['blade-echo-unescaped'];
+  }
+});
+
+const prismBlade = Prism.languages.blade;
+
+export { prismBlade as default };
