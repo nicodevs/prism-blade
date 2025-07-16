51986b4b98d6299ac5fe8ea03640b8739cea2e5b
Nico Devs <nico1485@gmail.com>
2025-07-16 00:21:16 -0300

    Use Rollup banner option for ESM

--- rollup.config.js
+++ rollup.config.js
@@ -21,7 +21,13 @@ export default [
     input: 'src/languages/prism-blade.js',
     output: {
       file: 'dist/prism-blade.js',
-      format: 'esm'
+      format: 'esm',
+      banner: `import Prism from 'prismjs';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-php';`
     }
   }
 ];
--- tests/prism-blade.test.js
+++ tests/prism-blade.test.js
@@ -1,5 +1,10 @@
 import { describe, expect, it } from 'vitest';
 import Prism from 'prismjs';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-php';
 import '../src/languages/prism-blade.js';
 
 // Helper to highlight Blade code
